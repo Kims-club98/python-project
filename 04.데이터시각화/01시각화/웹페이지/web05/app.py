837b0ef7ce2a539c07e76473fe02e79d6bfd9c29
Kims-club98 <naxen5611@naver.com>
2025-09-19 18:50:42 +0900

    9/19

--- 04.데이터시각화/01시각화/웹페이지/web05/app.py
+++ 04.데이터시각화/01시각화/웹페이지/web05/app.py
@@ -3,13 +3,37 @@ import pandas as pd
 import matplotlib.pyplot as plt
 from io import BytesIO
 
-
+plt.rc('font', family = 'Malgun Gothic')
+plt.rc('axes', unicode_minus = False)
 
 app=Flask(__name__,template_folder="temp") #template_folder = html을 모와놓은 폴더(template 렌더링 대상, html)
 
 @app.route("/")
 def health():
     return render_template("health.html")
+
+@app.route('/health/graph')
+def health_graph():
+
+    plt.rc('font',size=6)
+    df=pd.read_csv(f"{app.root_path}/data/인구수별공공의료기관수.csv")
+    word=request.args['word']
+    filt=df['시도군구'].str.contains(word) # word에 해당하는 단어가 "포함"되면 출력
+    df=df[filt]
+    if len(df) > 0:
+        df=df[:10]
+    df[filt]
+
+    plt.figure(figsize=(10,5))
+    plt.barh(df['시도군구'],df['count'])
+    plt.title('지역별 공공의료기관 수', size=20)
+
+    img=BytesIO()
+    plt.savefig(img,format='png')
+    plt.close()
+    img.seek(0)
+    return send_file(img, mimetype='image/png')
+
 
 @app.route("/health/data")
 def health_data():
@@ -29,27 +53,6 @@ def health_data():
     data={"count":count, "table":table}
     
     return data #table을 반환함
-
-@app.route('/health/graph')
-def health_graph():
-    df=pd.read_csv(f"{app.root_path}/data/인구수별공공의료기관수.csv")
-    word=request.args['word']
-    filt=df['시도군구'].str.contains(word) # word에 해당하는 단어가 "포함"되면 출력
-    df=df[filt]
-    if len(df) > 10:
-        df=df[:10]
-    df[filt]
-
-    plt.figure(figsize=(10,5))
-    plt.barh(df['시도군구'],df['count'])
-    plt.title('지역별 공공의료기관 수', size=20)
-    for idx, count in enumerate(df['count']):
-        plt.text(count+0.5, idx-0.05, f'{count}개', ha='center')
-    img=BytesIO()
-    plt.savefig(img,format='png')
-    plt.close()
-    img.seek(0)
-    return send_file(img, mimetype='image/png')
 
 
 if __name__=="__main__":
